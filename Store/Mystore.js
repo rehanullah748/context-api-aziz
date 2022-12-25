48a2343fe4ec88ea90fdd1abc16f1af17d1f4c42
rehanullah748 <102385527+rehanullah748@users.noreply.github.com>
2022-12-25 15:18:35 +0500

    Updated
    updated

--- Store/Mystore.js
+++ Store/Mystore.js
@@ -9,7 +9,27 @@ const [products, setProducts] = useState([])
 const [loader, setLoader] = useState(true)
 const [product, setProduct] = useState({})
 const [cart, setCart] = useState([])
-
+const Delete = (id) =>{
+  const filtered = cart.filter((cartitem)=>cartitem.id !== id)
+  setCart(filtered)
+}
+const Increase = (id)=> {
+  const item = cart.find((cartitem) => cartitem.id ===id)
+  const index = cart.findIndex((cartitem)=> cartitem.id === id)
+ 
+  item.Quantity += 1  
+  cart[index] = item
+    setCart([...cart])
+}
+const Decrease = (id) => {
+  const item = cart.find((cartitem) => cartitem.id ===id)
+  const index = cart.findIndex((cartitem)=> cartitem.id === id)
+ 
+  item.Quantity -= 1  
+  cart[index] = item
+    setCart([...cart])
+}
+    
 
 const addToCart = (item) => {
 const Filter = cart.find((myitem)=>myitem.id ===item.id)
@@ -54,7 +74,7 @@ useEffect(() => {
   }
   
 return (
-    <MystoreContext.Provider value={{user,count,setCount, Inc, products, setProducts,loader, product, getProduct, cart,  addToCart}}>
+    <MystoreContext.Provider value={{user,count,setCount, Inc, products, setProducts,loader, product, getProduct, cart, Increase, Decrease , addToCart, Delete}}>
     {props.children}
     
     </MystoreContext.Provider>
